047271bfdb773099049567ab4d078d6a48ea8584
saurabhjhastemlearning <saurabh.jha@stemlearning.in>
2025-05-22 15:55:16 +0530

    Removed << >>

--- index.js
+++ index.js
@@ -4,8 +4,5 @@ console.log("check 2")
 console.log("check 3")
 console.log("check 4")
 console.log("check 5")
-<<<<<<< Updated upstream
 console.log("check 6")
-=======
 console.log("Check 7")
->>>>>>> Stashed changes
