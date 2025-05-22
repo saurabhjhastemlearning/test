67be3b76aae511c457ac64b781dc38b56429b2ba
saurabhjhastemlearning <saurabh.jha@stemlearning.in>
2025-05-22 15:41:38 +0530

    Second Commit

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ console.log("check 2")
 console.log("check 3")
 console.log("check 4")
 console.log("check 5")
+<<<<<<< Updated upstream
+console.log("check 6")
+=======
+console.log("Check 7")
+>>>>>>> Stashed changes
